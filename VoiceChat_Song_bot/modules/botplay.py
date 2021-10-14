076f2a50b6e63d333f3aa8b204c46b9fe5975220
mpb00121 <84655588+mpb00121@users.noreply.github.com>
2021-10-14 19:09:40 +0530

    Update botplay.py

--- VoiceChat_Song_bot/modules/botplay.py
+++ VoiceChat_Song_bot/modules/botplay.py
@@ -536,7 +536,7 @@ async def play(_, message: Message):
         )
         file_name = get_file_name(audio)
         title = file_name
-        thumb_name = "https://telegra.ph/file/f6086f8909fbfeb0844f2.png"
+        thumb_name = "https://te.legra.ph/file/968c2b4dbb5b059e5acfb.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
