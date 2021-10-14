98143eaf20496522d3d6016fe7dd9ee5a1787c12
mpb00121 <84655588+mpb00121@users.noreply.github.com>
2021-10-14 18:22:53 +0530

    Add files via upload

---
+++ VoiceChat_Song_bot/functions/__init__.py
@@ -0,0 +1,5 @@
+from VoiceChat_Song_bot.functions.admins import set
+from VoiceChat_Song_bot.functions.admins import get
+from VoiceChat_Song_bot.functions.admins import admins
+
+__all__ = ["set", "get", "admins"]
